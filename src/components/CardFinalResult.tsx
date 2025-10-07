bff9a199376c692fd6914c3fd5683e5fc46c2102
monethip <monethip1299@gmail.com>
2025-10-07 16:20:04 +0700

    Update CardFinalResult component styling and adjust button width in globals.css

--- src/components/CardFinalResult.tsx
+++ src/components/CardFinalResult.tsx
@@ -15,41 +15,41 @@ const CardFinalResult: React.FC<TCardFinalResult> = ({
   return (
     <>
       <div style={{ marginBottom: "20px" }}>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: "20px",
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="96"
-            height="96"
-            viewBox="0 0 48 48"
-          >
-            <g
-              fill="none"
-              stroke="#81d95d"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="4"
-            >
-              <path d="M38 4H10a2 2 0 0 0-2 2v36a2 2 0 0 0 2 2h28a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2M17 30h14m-14 6h7" />
-              <path d="m30 13l-8 8l-4-4" />
-            </g>
-          </svg>
-
-          <h2 style={{ fontWeight: "bold", margin: "0 30px" }}>
-            Success
-          </h2>
-
-          <span></span>
-        </div>
-
         <div className="card-ui" style={{ marginBottom: "20px" }}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "20px",
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="48"
+              height="48"
+              viewBox="0 0 48 48"
+            >
+              <g
+                fill="none"
+                stroke="#22BB33"
+                strokeLinejoin="round"
+                strokeWidth="4"
+              >
+                <rect width="36" height="36" x="6" y="6" rx="3" />
+                <path
+                  strokeLinecap="round"
+                  d="M4 31h11l2 4h14l2-4h11m-2 5V26M6 36V26m11-7.385L22.6 24L33 14"
+                />
+              </g>
+            </svg>
+
+            <h2 style={{ fontSize: "20px", margin: "0 30px" }}>Success!</h2>
+
+            <span></span>
+          </div>
+
           <h2 style={{ fontWeight: "bold" }}>Final Results</h2>
           <p>
             <strong>Original Salary:</strong> {salary}
